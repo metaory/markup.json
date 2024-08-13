81b542de9210d7f2f02e0f7e5263bc6c288be5a6
metaory <metaory@gmail.com>
2024-08-13 23:26:58 +0800

    validation fix

--- src/cli.js
+++ src/cli.js
@@ -5,6 +5,9 @@ import { Readable } from 'node:stream'
 import assert from 'node:assert/strict'
 import transpile from './transpile.js'
 import validate from './validate.js'
+import { name } from '../package.json'
+
+process.title = name
 
 const [, , file] = argv
 if (file) assert.fail('FILE ARG NOT_IMPLEMENTED')
@@ -16,9 +19,9 @@ if (stream.readable === false) process.exit()
 Readable.from(stream)
   .reduce(async (acc, cur) => (acc += cur), '')
   .then(JSON.parse)
-  .catch(assert.ifError)
   .then(validate)
   .then(transpile)
   .then(console.log)
+  .catch(err => process.stderr.write(err.toString()))
 
 process.on('SIGINT', process.exit)
--- src/validate.js
+++ src/validate.js
@@ -1,21 +1,15 @@
 import assert from 'node:assert/strict'
 
 export default function (tpl) {
-  try {
-    assert.ok(Array.isArray(tpl), 'Array Expected')
-    assert.ok(tpl.length, 'Empty Array Recieved')
+  assert.ok(Array.isArray(tpl), 'Array Expected')
+  assert.ok(tpl.length, 'Empty Array Recieved')
 
-    const [root] = tpl
-    assert.ok(Array.isArray(root), 'Root Is NOT Array')
+  const [root] = tpl
+  assert.ok(Array.isArray(root), 'Root Is NOT Array')
 
-    const [tag, attr] = root
-    assert.ok(typeof tag === 'string', 'String Expected for Tag')
-    assert.ok(typeof attr === 'object', 'Object Expected for Attributes')
+  const [tag, attr] = root
+  assert.ok(typeof tag === 'string', 'String Expected for Tag')
+  assert.ok(typeof attr === 'object', 'Object Expected for Attributes')
 
-    return Promise.resolve(tpl)
-  } catch (err) {
-    console.error(err)
-    console.error('ERR', err.message)
-    process.exit(1)
-  }
+  return Promise.resolve(tpl)
 }
