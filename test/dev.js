485755b0256cea5f7eaadbd3eab69d394338c94c
metaory <metaory@gmail.com>
2024-09-14 17:16:14 +0800

    updated dev test files

--- test/dev.js
+++ test/dev.js
@@ -2,6 +2,7 @@ import { readFile, writeFile } from 'node:fs/promises'
 import assert from 'node:assert/strict'
 import transpile from '../src/transpile.js'
 import validate from '../src/validate.js'
+const { log, error } = console
 
 const [, , input] = process.argv
 
@@ -9,23 +10,16 @@ assert.ok(input, 'this dev file requires the input path as argument')
 assert.ok(input.endsWith('.json'), 'argument must be path to a json file')
 
 const content = JSON.parse(await readFile(input, { encoding: 'utf8' }))
-// const write = data => {
-//   console.log('data', '::', data, '<<<::')
-//   console.log('typeof data', '::', typeof data, '<<<::')
-//   writeFile(input.replace('.json', '.html', data))
-// }
-const write = data => writeFile(input.replace('.json', '.html'), data)
+const write = data => {
+  log(data)
+  writeFile(input.replace('.json', '.html'), data)
+}
+console.log('input', '::', input, '<<<::')
 
 assert.ok(content, 'missing json content')
 
-console.log('content', '::', content, '<<<::')
+log(content, '---')
 
-validate(content)
-  .then(transpile)
-  .then(write)
-  .then(console.log)
-  .catch(console.error)
-
-// validate(content).then(transpile).then(write).then(console.log).catch(console.error)
+validate(content).then(transpile).then(write).then(log).catch(error)
 
 // node --watch-path=src/transpile.js --watch-path=test/social.json test/dev.js test/social.json
